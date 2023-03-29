4d3670ed92d59f5e58f712cf6ca9de3992af444d
cwooldridge1 <christian.wooldridge@ucdenver.edu>
2023-03-29 11:08:47 -0600

    added enhancement

--- client/src/app/components/ui/Tile.tsx
+++ client/src/app/components/ui/Tile.tsx
@@ -1,7 +1,7 @@
 import { InteractiveContainerProps } from '../../types';
-import withInteractiveContainer from '../hocs/withInteractiveContainer';
+import withHorizontalSwipe from '../hocs/withHorizontalSwipe';
 
-const TileWrapper = (props: InteractiveContainerProps) => {
+const Tile = (props: InteractiveContainerProps) => {
   return (
     <div className="p-3 w-full bg-slate-200 hover:bg-white hover:backdrop-blur-md hover:bg-opacity-20 rounded-md shadow-lg flex items-center space-x-4 backdrop-filter backdrop-blur-md bg-opacity-20 mt-2">
       {props.children}
@@ -9,4 +9,4 @@ const TileWrapper = (props: InteractiveContainerProps) => {
   );
 };
 
-export default withInteractiveContainer(TileWrapper);
+export default withHorizontalSwipe(Tile);
