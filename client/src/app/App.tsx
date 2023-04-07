850b56f87f8e76bcb216766ddbf8d25579434349
cwooldridge1 <christian.wooldridge@ucdenver.edu>
2023-04-07 16:17:47 -0600

    removed electron and upgraded docker-compose

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,6 @@ import './App.scss';
 import Layout from './components/Layout';
 import AudioStreamHeader from './features/AudioStreamer/AudioStreamHeader';
 
-// const { app } = window.require('@electron/remote');
 function App() {
   return (
     <>
